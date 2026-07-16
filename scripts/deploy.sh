f70e3a0ffed0f583010f3a853daf28406cb829ea
Jake Hash <1508098+jhash@users.noreply.github.com>
2026-07-16 13:46:58 -0700

    Use oci-ip helper instead of expecting var to be there

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --tag jhash14/tabitha:latest . && ssh deploy@$OCI_SERVER_IP 'docker pull jhash14/tabitha:latest && docker stack deploy -c /home/deploy/stacks/tabitha/stack.yml tabitha'
+docker buildx build --push --tag jhash14/tabitha:latest . && ssh deploy@$(oci-ip) 'docker pull jhash14/tabitha:latest && docker stack deploy -c /home/deploy/stacks/tabitha/stack.yml tabitha'
